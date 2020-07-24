bef819b4219dbe265005679731a4cdcab9b6435a
XIEAN2 <andreatwelve@sina.cn>
2020-07-24 15:22:45 +0800

    [guessnumber] [andrea] test: add should_return_ticket_when_park_given_car test

---
+++ src/main/java/com/oocl/cultivation/ParkLot.java
@@ -0,0 +1,10 @@
+package com.oocl.cultivation;
+
+import com.oocl.cultivation.test.Car;
+
+public class ParkLot {
+    public CarTicket park(Car car) {
+        return new CarTicket();
+    }
+
+}
--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,26 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkLot;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParkingBoyFacts {
     @Test
     void write_your_first_test() {
 
+    }
+
+    @Test
+    void should_return_ticket_when_park_given_car() {
+        //given
+        //when
+        //CarTicket carTicket =
+        ParkLot parkLot = new ParkLot();
+        Car car = new Car();
+        CarTicket carTicket = parkLot.park(car);
+        //then
+        assertNotNull(carTicket);
     }
 }
