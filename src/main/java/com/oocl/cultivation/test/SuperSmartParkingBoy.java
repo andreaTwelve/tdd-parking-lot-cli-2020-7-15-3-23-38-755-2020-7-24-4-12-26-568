7053b4a99261014831574375b9370b69ba9f7bbd
andrea <andreatwelve@sina.cn>
2020-07-27 22:58:38 +0800

    [story] [andrea] refactor: delete unused method of gcd

--- src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
@@ -32,20 +32,5 @@ public class SuperSmartParkingBoy extends SmartParkingBoy {
         return super.getMaxRestCapacity(parkLotRestCapacity);
     }
 
-    public int gcd(int n, int m) {
-        int temp,r;
-        if(n<m)
-        {
-            temp=n;
-            n=m;
-            m=temp;
-        }
-        while(m!=0)
-        {
-            r=n%m;
-            n=m;
-            m=r;
-        }
-        return n;
-    }
+
 }
